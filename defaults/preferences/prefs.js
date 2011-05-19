ab6e9c8c5bb34762b4480cbef2b8ff83cc25f389
Per Ångström <github@autark.se>
2011-05-19 11:01:00 +0200

    An overview of the defaults.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,8 +1,8 @@
 pref("extensions.html5validator.validatorURL", "http://localhost:8888/");
 pref("extensions.html5validator.domainsWhitelist", "");
-pref("extensions.html5validator.restrictToWhitelist", true);
+pref("extensions.html5validator.restrictToWhitelist", false);
 pref("extensions.html5validator.displayResultsInTab", false);
-pref("extensions.html5validator.useTrigger", false);
+pref("extensions.html5validator.useTrigger", true);
 pref("extensions.html5validator.debug", false);
 pref("extensions.html5validator.ignoreXHTMLErrors", false);
 pref("extensions.html5validator.allowAccessibilityFeatures", false);
